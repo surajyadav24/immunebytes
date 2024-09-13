176693a3a7f459010ebbdf352ed2374d9372d160
suraj kumar yadav <84555072+surajyadav24@users.noreply.github.com>
2024-09-13 15:24:36 +0530

    LOgo added

--- src/components/Servicescomponent/index.jsx
+++ src/components/Servicescomponent/index.jsx
@@ -7,14 +7,14 @@ const ServicesComponent = ({ heading, paragraphtext, imageSrc, className }) => {
     <div className={`services-component ${className}`}>
       <div className="container">
         <div className="row">
-          <div className="col-lg-8 md-6 col-sm-6 col-xs-6 left-serv">
+          <div className="col-lg-7 md-6 col-sm-6 col-xs-6 left-serv">
             <div className="service-discription">
               <h2>{heading}</h2>
               <p>{paragraphtext}</p>
               <PrimaryBtn text="View More" />
             </div>
           </div>
-          <div className="col-lg-4 md-6 col-sm-6 col-xs-6 right-serv">
+          <div className="col-lg-5 md-6 col-sm-6 col-xs-6 right-serv">
             <div className="service-img">
               <img src={imageSrc} alt="Services" className="services-image" />
             </div>
