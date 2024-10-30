cfadcbeb9178730bc26132c7b948118105729e9b
suraj kumar yadav <7suraj8yadav6@gmail.com>
2024-10-30 11:28:27 +0530

    tab home page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Logo from '../../assets/images/logos/Logo.svg'
-import Formpopup from '../Formpopup'
+import Logo from '../../assets/images/logos/Logo.svg';
+import Formpopup from '../Formpopup';
+
 function Header() {
   const [isOpen, setIsOpen] = useState(false); // State to track menu open/close
 
@@ -14,45 +15,35 @@ function Header() {
     <>
       <header className="text-white p-3">
         <div className="container d-flex justify-content-between align-items-center p-0">
-          
-
-    
           <div className="h3 font-weight-bold logo">
-          <a href="/">  <img src={Logo} alt="" /></a>
+            <a href="/"><img src={Logo} alt="" /></a>
           </div>
-          <nav className="desktop-nav d-none d-md-block">
+          
+          {/* Desktop Nav: Hidden on mobile/tablet */}
+          <nav className="desktop-nav d-none d-lg-block">
             <ul className="nav">
-            <li className="nav-item">
-                <a href="/Home" className="nav-link text-white">
-             Services
-                </a>
+              <li className="nav-item">
+                <a href="/Home" className="nav-link text-white">Services</a>
               </li>
               <li className="nav-item">
-                <a href="/portfolio" className="nav-link text-white">
-                 Portfolio
-                </a>
-              </li>
-           
-              <li className="nav-item">
-                <a href="/about-us" className="nav-link text-white">
-                  About Us
-                </a>
+                <a href="/portfolio" className="nav-link text-white">Portfolio</a>
               </li>
               <li className="nav-item">
-                <a href="" className="nav-link text-white">
-             BLog
-                </a>
+                <a href="/about-us" className="nav-link text-white">About Us</a>
+              </li>
+              <li className="nav-item">
+                <a href="/blog" className="nav-link text-white">Blog</a>
               </li>
             </ul>
           </nav>
 
-          {/* "Request Audit" Button Always Visible */}
-       <div className="desktop-btn-request">
-       <Formpopup/>
-       </div>
+          {/* Request Button */}
+          <div className="desktop-btn-request">
+            <Formpopup />
+          </div>
 
           {/* Hamburger Menu for Mobile & Tablet */}
-          <div className="hamburger d-md-none" onClick={toggleMenu}>
+          <div className="hamburger d-lg-none" onClick={toggleMenu}>
             <span className="bar"></span>
             <span className="bar"></span>
             <span className="bar"></span>
@@ -61,30 +52,22 @@ function Header() {
 
         {/* Mobile/Tablet Menu */}
         {isOpen && (
-          <nav className="mobile-nav d-md-none">
+          <nav className="mobile-nav d-lg-none">
             <ul className="nav flex-column">
-            <li className="nav-item">
-                <a href="/Home" className="nav-link text-white">
-             Services
-                </a>
+              <li className="nav-item">
+                <a href="/Home" className="nav-link text-white">Services</a>
               </li>
               <li className="nav-item">
-                <a href="/portfolio" className="nav-link text-white">
-                 Portfolio
-                </a>
+                <a href="/portfolio" className="nav-link text-white">Portfolio</a>
               </li>
               <li className="nav-item">
-                <a href="/about-us" className="nav-link text-white">
-                  About Us
-                </a>
+                <a href="/about-us" className="nav-link text-white">About Us</a>
               </li>
               <li className="nav-item">
-                <a href="" className="nav-link text-white">
-             BLog
-                </a>
+                <a href="/blog" className="nav-link text-white">Blog</a>
               </li>
               <li className="nav-item">
-              <Formpopup/>
+                <Formpopup />
               </li>
             </ul>
           </nav>
